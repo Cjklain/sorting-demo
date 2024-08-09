65c686eefce3ab776d3e41c898dc30f317d8eb4f
CJ <karolmmm01@gmail.com>
2024-08-09 20:58:07 +0200

    Add value handler

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log('qwe12e')
+const valueRange = document.querySelector("#value-range")! as HTMLInputElement;
+const valueNumber = document.querySelector("#value-number")! as HTMLInputElement;
+
+const MAXVALUE = 100;
+
+const handleValueChange = (e: Event, element: HTMLInputElement) => {
+  const target = e.target as HTMLInputElement;
+
+  if (+target.value > MAXVALUE) {
+    target.value = "" + MAXVALUE;
+  }
+
+  if (target) {
+    element.value = target.value;
+  }
+};
+
+valueRange.addEventListener("input", (e) => handleValueChange(e, valueNumber));
+valueNumber.addEventListener("input", (e) => handleValueChange(e, valueRange));
