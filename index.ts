8fad8d136230dd820fbb8cadab8bec5cdebc21ae
CJ <karolmmm01@gmail.com>
2024-08-18 23:04:29 +0200

    Add function to handle selecting algorithm

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 const valueRange = document.querySelector("#value-range")! as HTMLInputElement;
 const valueNumber = document.querySelector("#value-number")! as HTMLInputElement;
 const board = document.querySelector(".board")! as HTMLInputElement;
+const selectButtons = document.querySelectorAll(".algo")! as NodeListOf<HTMLButtonElement>;
+const runButton = document.querySelector(".run")! as HTMLButtonElement;
 
 const MAXVALUE = 100;
 
@@ -21,13 +23,26 @@ const handleValueChange = (e: Event, element: HTMLInputElement) => {
   }
 };
 
+const handleButtonClick = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+
+  // one button can has selected class
+  selectButtons.forEach((button) => {
+    if (button.classList.contains("selected") && button.id !== target.id) {
+      button.classList.remove("selected");
+    }
+  });
+
+  target.classList.toggle("selected");
+};
+
 const generateElements = () => {
   board.replaceChildren();
   let elements: Array<HTMLDivElement> = [];
 
   for (let i = 0; i < +valueRange.value; i++) {
     const el = document.createElement("div");
-    el.classList.add("test");
+    el.classList.add("column");
     el.style.height = `${10 + i * 4}px`;
     elements.push(el);
   }
@@ -51,5 +66,7 @@ valueNumber.addEventListener("input", (e) => handleValueChange(e, valueRange));
 
 valueRange.addEventListener("input", generateElements);
 valueNumber.addEventListener("input", generateElements);
+selectButtons.forEach((button) => button.addEventListener("click", (e) => handleButtonClick(e)));
+// runButton.addEventListener("click", handleRun);
 
 main();
